c8611c988cc31f467950142d89c199c9c7e15474
ivan <givan1010@gmail.com>
2021-04-28 01:09:45 +0500

    init

--- src/scripts/components/App/index.js
+++ src/scripts/components/App/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import style from './App.scss'
+import style from "./App.scss";
+import Header from "components/Header";
 
 export default function App() {
-    return (
-        <div className={style.main}>
+  return (
+    <>
+        <Header/>
 
+      <div className={style.main}>
         <a href=""> Hello World!</a>
-    </div>
-)
+      </div>
+    </>
+  );
 }
---
+++ src/scripts/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from "components/App/App.scss";
+
+export default function Header () {
+
+    return (
+        <header>123</header>
+    )
+}
